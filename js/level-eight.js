027d6d774f9dd2d940944de11cc1476af153a554
joe-software <joewebdev@outlook.com>
2023-10-05 02:45:02 +0100

    add Level 8 challenge

--- js/level-eight.js
+++ js/level-eight.js
@@ -1,3 +1,7 @@
+// https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript reverse 2 items in an array with 2 items. I shouldve just used '.rev()' on the split 'str'
+
+nameShuffler = str =>  [str.split(' ')[1], str.split(' ')[0]].join(' ')
+
 // https://www.codewars.com/kata/5aa736a455f906981800360d/train/javascript first and last letters from each prop must be the same
 
 function feast(beast, dish) {
