dfc09cf468bef340436b288f639e10a7da047ecb
joe-software <joewebdev@outlook.com>
2023-10-06 23:02:48 +0100

    add Level 8 challenge

--- js/level-eight.js
+++ js/level-eight.js
@@ -1,3 +1,32 @@
+//  https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript - simple challenge to return the number of days in a month
+
+function howManydays(month){
+  var days;
+  switch (month){
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        days = 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:  
+        days = 30;
+        break;
+      case 2:  
+        days = 28;
+        break;
+  
+  }
+  return days;
+}
+
+
 // https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript reverse 2 items in an array with 2 items. I shouldve just used '.rev()' on the split 'str'
 
 nameShuffler = str =>  [str.split(' ')[1], str.split(' ')[0]].join(' ')
