9e1d471a78e226505289e367fac34ff42f2e276d
joe-software <joewebdev@outlook.com>
2023-11-12 23:42:56 +0000

    level 6 challenge

--- js/level-six.js
+++ js/level-six.js
@@ -1,3 +1,10 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+
+function createPhoneNumber(numbers){
+
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+ }
 // https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/javascript
 
 function deleteNth(arr,n){
