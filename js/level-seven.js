dcfece7dfd2bb2b6d21ab8b1204dccc19e757669
joe-software <joewebdev@outlook.com>
2024-09-20 16:14:48 +0100

    Level 7 challenge

--- js/level-seven.js
+++ js/level-seven.js
@@ -1,3 +1,9 @@
+// https://www.codewars.com/kata/514a677421607afc99000002/train/javascript
+
+function getNames(data){
+  return data.map( item => item.name);
+}
+
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
 
 function disemvowel(str) {
