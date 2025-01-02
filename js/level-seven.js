01a3672296e4d0a502e3beccb35aed6392955a2e
joe-software <joewebdev@outlook.com>
2025-01-02 23:10:21 +0000

    Add Level 7 challenge

--- js/level-seven.js
+++ js/level-seven.js
@@ -1,3 +1,14 @@
+// https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/javascript
+
+var number = function(busStops){
+  let total= 0;
+  busStops.forEach(item => {
+    total = total + item[0]
+    total = total - item[1]
+  })
+  return total
+}
+
 // https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
 
 const binaryArrayToNumber = arr => {
